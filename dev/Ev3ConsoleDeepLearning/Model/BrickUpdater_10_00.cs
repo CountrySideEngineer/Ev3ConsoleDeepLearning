d593711491a9582c906b17206d17943b29167f83
DeepSleepEngineer <deep-sleeper@nify.com>
2018-02-11 22:11:24 +0900

    Add BrickUpdater. -Diver BrickUpdater classes from Ev3Controller.

---
+++ dev/Ev3ConsoleDeepLearning/Model/BrickUpdater_10_00.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ev3Controller.Ev3Command;
+using System.Diagnostics;
+
+namespace Ev3Controller.Model
+{
+    public class BrickUpdater_10_00 : BrickUpdater
+    {
+        /// <summary>
+        /// Update motor device information of Ev3 Brick by GetMotorPower command,
+        /// especially motor output power.
+        /// </summary>
+        /// <param name="Command"></param>
+        /// <param name="Brick"></param>
+        public override void Update(ACommand Command, Ev3Brick Brick)
+        {
+            Debug.Assert(Command != null);
+            Debug.Assert(Brick != null);
+            //Nothing to check in this response data.
+        }
+    }
+}
